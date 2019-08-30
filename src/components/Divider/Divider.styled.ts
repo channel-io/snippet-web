0c954ed513a9983b6617c73421c5eb914d3017a5
hahnlee <hanlee.dev@gmail.com>
2019-08-30 14:52:33 +0900

    Fix Divider border style

--- src/components/Divider/Divider.styled.ts
+++ src/components/Divider/Divider.styled.ts
@@ -5,6 +5,7 @@ import styled from 'styled-components'
 import Colors from 'Styles/colors'
 
 export const Divider = styled.hr`
+  border: none;
   border-top: 1px solid ${Colors.Grey300};
   margin: 0 0 8px 0;
 `
